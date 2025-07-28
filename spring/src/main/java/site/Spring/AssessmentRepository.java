0ebf66dd39f45daf251140f76a4f585e8dbd5bf4
msf-navarro <msf.navarro@gmail.com>
2025-07-28 19:23:52 -0300

    Delete spring/src/main/java/site/Spring/AssessmentRepository.java

--- spring/src/main/java/site/Spring/AssessmentRepository.java
+++
@@ -1,7 +0,0 @@
-package site.Spring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@org.springframework.stereotype.Repository
-public interface AssessmentRepository extends JpaRepository<Exam, Long> {
-}
