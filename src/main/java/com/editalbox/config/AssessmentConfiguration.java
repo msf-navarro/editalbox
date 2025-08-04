a66384574bc95e2368ec3ab1516ebe3036fa39e5
msf-navarro <msf.navarro@gmail.com>
2025-08-04 15:58:50 -0300

    August02 - debug rollback 2

--- src/main/java/com/editalbox/config/AssessmentConfiguration.java
+++ src/main/java/com/editalbox/config/AssessmentConfiguration.java
@@ -1,57 +1,57 @@
-package com.editalbox.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
-import com.editalbox.model.Content;
-import com.editalbox.repository.ContentRepository;
-import com.editalbox.model.Exam;
-import com.editalbox.repository.ExamRepository;
-
-@Configuration
-public class AssessmentConfiguration implements CommandLineRunner{
-
-    private final ExamRepository examRepository;
-    private final ContentRepository contentRepository;
-
-    @Autowired
-    AssessmentConfiguration(ExamRepository examRepository,
-                            ContentRepository contentRepository){
-        this.examRepository = examRepository;
-        this.contentRepository = contentRepository;
-    }
-
-    @Override
-    public void run(String... args) throws Exception{
-
-        Exam examA = new Exam(
-                "ExampleA (2009)",
-                2009,
-                0);
-        examRepository.save(examA);
-
-        Exam examB = new Exam(
-                "ExampleB (2007)",
-                2007,
-                0);
-        examRepository.save(examB);
-
-        Content contentA1 = new Content(
-                examA,
-                "statistics",
-                "simple probability");
-        contentRepository.save(contentA1);
-
-        Content contentA2 = new Content(
-                examA,
-                "statistics",
-                "conditional probability");
-        contentRepository.save(contentA2);
-
-        Content contentB1 = new Content(
-                examB,
-                "mathematics",
-                "linear algebra");
-        contentRepository.save(contentB1);
-    }
-}
+//package com.editalbox.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Configuration;
+//import com.editalbox.model.Content;
+//import com.editalbox.repository.ContentRepository;
+//import com.editalbox.model.Exam;
+//import com.editalbox.repository.ExamRepository;
+//
+//@Configuration
+//public class AssessmentConfiguration implements CommandLineRunner {
+//
+//    private final ExamRepository examRepository;
+//    private final ContentRepository contentRepository;
+//
+//    @Autowired
+//    AssessmentConfiguration(ExamRepository examRepository,
+//                            ContentRepository contentRepository) {
+//        this.examRepository = examRepository;
+//        this.contentRepository = contentRepository;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        Exam examA = new Exam(
+//                "ExampleA (2009)",
+//                2009,
+//                0);
+//        examRepository.save(examA);
+//
+//        Exam examB = new Exam(
+//                "ExampleB (2007)",
+//                2007,
+//                0);
+//        examRepository.save(examB);
+//
+//        Content contentA1 = new Content(
+//                examA,
+//                "statistics",
+//                "simple probability");
+//        contentRepository.save(contentA1);
+//
+//        Content contentA2 = new Content(
+//                examA,
+//                "statistics",
+//                "conditional probability");
+//        contentRepository.save(contentA2);
+//
+//        Content contentB1 = new Content(
+//                examB,
+//                "mathematics",
+//                "linear algebra");
+//        contentRepository.save(contentB1);
+//    }
+//}
---
+++ src/main/java/com/editalbox/config/ExamConfig.java
@@ -0,0 +1,39 @@
+package com.editalbox.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import com.editalbox.model.Exam;
+import com.editalbox.repository.ExamRepository;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+//Needs to run before ContentConfig,
+//otherwise it leads to an exception:
+//"not-null property references a null or transient value"
+@Order(1)
+public class ExamConfig implements CommandLineRunner {
+
+    private final ExamRepository examRepository;
+
+    @Autowired
+    ExamConfig(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Exam examA = new Exam(
+                "ExampleA (2009)",
+                2009,
+                0);
+        examRepository.save(examA);
+
+        Exam examB = new Exam(
+                "ExampleB (2007)",
+                2007,
+                0);
+        examRepository.save(examB);
+
+    }
+}
